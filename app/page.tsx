c0a6a61219be60a5c1defe91117f1932bd398998
jayraj-kl <jayrajkladkat@gmail.com>
2025-02-14 00:39:32 +0530

    v1

---
+++ app/page.tsx
@@ -0,0 +1,20 @@
+import { Chat } from "@/components/chat";
+import { DarkModeToggle } from "@/components/dark-mode-toggle";
+
+export default function Home() {
+  return (
+    <>
+      <main className="relative container flex min-h-screen flex-col mx-auto max-w-4xl">
+        <div className=" p-4 flex h-14 items-center justify-between supports-backdrop-blur:bg-background/60 sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur">
+          <span className="font-bold">pdf-chat-ai-sdk</span>
+          <DarkModeToggle />
+        </div>
+        <div className="flex flex-1 py-4">
+          <div className="w-full">
+            <Chat />
+          </div>
+        </div>
+      </main>
+    </>
+  );
+}
---
+++ lib/utils.ts
@@ -0,0 +1,60 @@
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+import { Message } from "ai";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+export function formattedText(inputText: string) {
+  return inputText
+    .replace(/\n+/g, " ")
+    .replace(/(\w) - (\w)/g, "$1$2")
+    .replace(/\s+/g, " ");
+}
+
+export function delay(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+// Default UI Message
+export const initialMessages: Message[] = [
+  {
+    role: "assistant",
+    id: "0",
+    content:
+      "Hi! I am your PDF assistant. I am happy to help with your questions about your PDF about German law.",
+  },
+];
+
+interface Data {
+  sources: string[];
+}
+
+export function scrollToBottom(containerRef: React.RefObject<HTMLDivElement>) {
+  if (containerRef.current) {
+    const lastMessage = containerRef.current.lastElementChild;
+    if (lastMessage) {
+      const scrollOptions: ScrollIntoViewOptions = {
+        behavior: "smooth",
+        block: "end",
+      };
+      lastMessage.scrollIntoView(scrollOptions);
+    }
+  }
+}
+
+interface Data {
+  sources: string[];
+}
+
+// Maps the sources with the right ai-message
+export const getSources = (data: Data[], role: string, index: number) => {
+  if (role === "assistant" && index >= 2 && (index - 2) % 2 === 0) {
+    const sourcesIndex = (index - 2) / 2;
+    if (data[sourcesIndex] && data[sourcesIndex].sources) {
+      return data[sourcesIndex].sources;
+    }
+  }
+  return [];
+};
